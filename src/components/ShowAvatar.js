4a0400abd575132d46dadadcff1ddb151398d6bb
Alexander Vikenfalk <alex@petitcode.com>
2018-02-13 23:58:19 +0100

    made ShowAvatar component to a stateless functional component instead

--- src/components/ShowAvatar.js
+++ src/components/ShowAvatar.js
@@ -1,28 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "../styles/showAvatar.css";
 
-export default class ShowAvatar extends Component {
-
-  handleKeyPress = (event) => {
-    if(event.key === 'Enter'){
+// export default class ShowAvatar extends Component {
+  const handleKeyPress = event => {
+    if (event.key === "Enter") {
       this.props.showPopup();
     }
-  }
+  };
 
-  render() {
-    const avatar = this.props.currentAvatar;  
-    // Renders the selected avatar
-    return (
-      <img
-        src={process.env.PUBLIC_URL + avatar.src}
-        key={avatar.id}
-        alt={avatar.label}
-        className="avatar currentAvatar"
-        onClick={() => this.props.showPopup()}
-        onKeyPress={(event) => this.handleKeyPress(event)}
-        tabIndex="0"
-      />
-    );
-  }
-}
+// Renders the selected avatar
+const showAvatar = props => {
+  const avatar = props.currentAvatar;
+  return (
+    <img
+      src={process.env.PUBLIC_URL + avatar.src}
+      key={avatar.id}
+      alt={avatar.label}
+      className="avatar currentAvatar"
+      onClick={() => props.showPopup()}
+      onKeyPress={event => handleKeyPress(event)}
+      tabIndex="0"
+    />
+  );
+};
+
+export default showAvatar;
